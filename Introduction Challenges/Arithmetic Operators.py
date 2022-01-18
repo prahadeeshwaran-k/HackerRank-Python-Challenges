fc863f3aa223378504398bece5e70d4c52522142
Prahadeeshwaran <95561624+prahadeeshwaran-k@users.noreply.github.com>
2022-01-18 21:13:08 +0530

    Level 1

---
+++ Introduction Challenges/Arithmetic Operators.py
@@ -0,0 +1,14 @@
+#Arithmetic Operators
+
+#Task
+#The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+#1.The first line contains the sum of the two numbers.
+#2.The second line contains the difference of the two numbers (first - second).
+#3.The third line contains the product of the two numbers.
+
+a = int(input("Enter a number A: "))
+b = int(input("Enter a number B:  "))
+print("Sum of ",a, " and ",b," is: ",a + b)
+print("Sub of ",a, " and ",b," is: ",a - b)
+print("Product of ",a, " and ",b," is: ",a * b)
